fc4a9c2f5780809ec002e18fbc2bac5e195a2e7c
leberKleber <max.marche@live.de>
2020-01-20 10:42:35 +0100

    hello world

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tarent/gomulocity
+
+go 1.13
---
+++ hello_test.go
@@ -0,0 +1,10 @@
+package gomulocity
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHelloWorld(t *testing.T) {
+	fmt.Print("Hello World")
+}
