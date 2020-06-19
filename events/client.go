d1a753333caf02c1673582ffc511699ec96bb336
Christian Schmidt <c.schmidt@tarent.de>
2020-06-19 17:00:28 +0200

    Adding a http client facade. We should consolidate this.

---
+++ events/client.go
@@ -0,0 +1,62 @@
+package gomulocity_event
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Client struct {
+	HTTPClient *http.Client
+	BaseURL    string
+	Username   string
+	Password   string
+}
+
+func (client *Client) post(path string, body string) ([]byte, error) {
+	url := client.BaseURL + path
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBufferString(body))
+	if err != nil {
+		log.Printf("Error: While creating a request: %s", err.Error())
+		return nil, err
+	}
+
+	return client.request(req)
+}
+
+func (client *Client) get(path string) ([]byte, error) {
+	url := client.BaseURL + path
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Printf("Error: While creating a request: %s", err.Error())
+		return nil, err
+	}
+
+	return client.request(req)
+}
+
+func (client *Client) request(req *http.Request) ([]byte, error) {
+	log.Printf("HTTP %s on URL %s", req.Method, req.URL)
+
+	req.SetBasicAuth(client.Username, client.Password)
+
+	resp, err := client.HTTPClient.Do(req)
+	if err != nil {
+		log.Printf("An error occured: %s", err.Error())
+		return nil, err
+	}
+	log.Printf("Got status %d", resp.StatusCode)
+	defer resp.Body.Close()
+
+	result, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error while reading from stream: %s", err.Error())
+		return nil, err
+	}
+
+	log.Printf("Debug: Response body was: %s", result)
+	return result, nil
+}
