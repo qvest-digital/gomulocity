ed1c5fa401cc4eb04ffc6ba651907dd056d706db
Holger Schmitz <h.schmitz@tarent.de>
2020-06-23 14:23:37 +0200

    createMeasurement

--- measurements/measurements.go
+++ measurements/measurements.go
@@ -1,6 +1,7 @@
 package measurements
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -27,10 +28,52 @@ func (c Client) getMeasurement(id string) (models.Measurement, error) {
 	if err != nil {
 		return models.Measurement{}, fmt.Errorf("failed to initialize rest request: %w", err)
 	}
-	req.Header.Add("Accept", CONTENT_TYPE_MEASUREMENT)
+	req.Header.Add("Accept", ACCEPT_MEASUREMENT)
 	req.SetBasicAuth(c.Username, c.Password)
 
-	//TODO: Add pagination
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return models.Measurement{}, fmt.Errorf("failed to execute rest request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		switch resp.StatusCode {
+		case http.StatusUnauthorized:
+			return models.Measurement{}, generic.BadCredentialsErr
+		case http.StatusForbidden:
+			return models.Measurement{}, generic.AccessDeniedErr
+		default:
+			return models.Measurement{}, fmt.Errorf("received an unexpected status code: %v", resp.StatusCode)
+		}
+	}
+
+	measurementFromAPI := models.Measurement{}
+
+	if err = json.NewDecoder(resp.Body).Decode(&measurementFromAPI); err != nil {
+		return models.Measurement{}, fmt.Errorf("failed to unmarshal response body: %w", err)
+	}
+
+	return measurementFromAPI, nil
+}
+
+func (c Client) createMeasurement(measurement models.Measurement) (models.Measurement, error) {
+	body, err := json.Marshal(measurement)
+	if err != nil {
+		return models.Measurement{}, fmt.Errorf("failed to marshal request body: %w", err)
+	}
+	req, err := http.NewRequest(
+		http.MethodPost,
+		fmt.Sprintf("%v%v", c.BaseURL, MEASUREMENTS_API),
+		bytes.NewReader(body),
+	)
+	if err != nil {
+		return models.Measurement{}, fmt.Errorf("failed to initialize rest request: %w", err)
+	}
+	req.Header.Add("Accept", ACCEPT_MEASUREMENT)
+	req.Header.Add("Content-Type", CONTENT_TYPE_MEASUREMENT)
+	req.SetBasicAuth(c.Username, c.Password)
+
 	resp, err := c.HTTPClient.Do(req)
 	if err != nil {
 		return models.Measurement{}, fmt.Errorf("failed to execute rest request: %w", err)
