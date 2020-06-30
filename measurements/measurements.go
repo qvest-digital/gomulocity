6b7a88a583cf98acd1b1df6d5c10a10d08f41223
Holger Schmitz <h.schmitz@tarent.de>
2020-06-30 11:59:22 +0200

    add deleteMeasurement

--- measurements/measurements.go
+++ measurements/measurements.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 
 	"github.com/tarent/gomulocity/generic"
-	"github.com/tarent/gomulocity/models"
 )
 
 const (
@@ -19,48 +18,48 @@ const (
 	MEASUREMENTS_API = "measurement/measurements/"
 )
 
-func (c Client) getMeasurement(id string) (models.Measurement, error) {
+func (c Client) getMeasurement(id string) (Measurement, error) {
 	req, err := http.NewRequest(
 		http.MethodGet,
 		fmt.Sprintf("%v%v", c.BaseURL, MEASUREMENTS_API),
 		nil,
 	)
 	if err != nil {
-		return models.Measurement{}, fmt.Errorf("failed to initialize rest request: %w", err)
+		return Measurement{}, fmt.Errorf("failed to initialize rest request: %w", err)
 	}
 	req.Header.Add("Accept", ACCEPT_MEASUREMENT)
 	req.SetBasicAuth(c.Username, c.Password)
 
 	resp, err := c.HTTPClient.Do(req)
 	if err != nil {
-		return models.Measurement{}, fmt.Errorf("failed to execute rest request: %w", err)
+		return Measurement{}, fmt.Errorf("failed to execute rest request: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		switch resp.StatusCode {
 		case http.StatusUnauthorized:
-			return models.Measurement{}, generic.BadCredentialsErr
+			return Measurement{}, generic.BadCredentialsErr
 		case http.StatusForbidden:
-			return models.Measurement{}, generic.AccessDeniedErr
+			return Measurement{}, generic.AccessDeniedErr
 		default:
-			return models.Measurement{}, fmt.Errorf("received an unexpected status code: %v", resp.StatusCode)
+			return Measurement{}, fmt.Errorf("received an unexpected status code: %v", resp.StatusCode)
 		}
 	}
 
-	measurementFromAPI := models.Measurement{}
+	measurementFromAPI := Measurement{}
 
 	if err = json.NewDecoder(resp.Body).Decode(&measurementFromAPI); err != nil {
-		return models.Measurement{}, fmt.Errorf("failed to unmarshal response body: %w", err)
+		return Measurement{}, fmt.Errorf("failed to unmarshal response body: %w", err)
 	}
 
 	return measurementFromAPI, nil
 }
 
-func (c Client) createMeasurement(measurement models.Measurement) (models.Measurement, error) {
+func (c Client) createMeasurement(measurement Measurement) (Measurement, error) {
 	body, err := json.Marshal(measurement)
 	if err != nil {
-		return models.Measurement{}, fmt.Errorf("failed to marshal request body: %w", err)
+		return Measurement{}, fmt.Errorf("failed to marshal request body: %w", err)
 	}
 	req, err := http.NewRequest(
 		http.MethodPost,
@@ -68,7 +67,7 @@ func (c Client) createMeasurement(measurement models.Measurement) (models.Measur
 		bytes.NewReader(body),
 	)
 	if err != nil {
-		return models.Measurement{}, fmt.Errorf("failed to initialize rest request: %w", err)
+		return Measurement{}, fmt.Errorf("failed to initialize rest request: %w", err)
 	}
 	req.Header.Add("Accept", ACCEPT_MEASUREMENT)
 	req.Header.Add("Content-Type", CONTENT_TYPE_MEASUREMENT)
@@ -76,25 +75,100 @@ func (c Client) createMeasurement(measurement models.Measurement) (models.Measur
 
 	resp, err := c.HTTPClient.Do(req)
 	if err != nil {
-		return models.Measurement{}, fmt.Errorf("failed to execute rest request: %w", err)
+		return Measurement{}, fmt.Errorf("failed to execute rest request: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		switch resp.StatusCode {
 		case http.StatusUnauthorized:
-			return models.Measurement{}, generic.BadCredentialsErr
+			return Measurement{}, generic.BadCredentialsErr
 		case http.StatusForbidden:
-			return models.Measurement{}, generic.AccessDeniedErr
+			return Measurement{}, generic.AccessDeniedErr
 		default:
-			return models.Measurement{}, fmt.Errorf("received an unexpected status code: %v", resp.StatusCode)
+			return Measurement{}, fmt.Errorf("received an unexpected status code: %v", resp.StatusCode)
 		}
 	}
 
-	measurementFromAPI := models.Measurement{}
+	measurementFromAPI := Measurement{}
 
 	if err = json.NewDecoder(resp.Body).Decode(&measurementFromAPI); err != nil {
-		return models.Measurement{}, fmt.Errorf("failed to unmarshal response body: %w", err)
+		return Measurement{}, fmt.Errorf("failed to unmarshal response body: %w", err)
+	}
+
+	return measurementFromAPI, nil
+}
+
+func (c Client) GetMeasurements(resultSize int) (Measurement, error) {
+	req, err := http.NewRequest(
+		http.MethodGet,
+		fmt.Sprintf("%v%v", c.BaseURL, MEASUREMENTS_API),
+		nil,
+	)
+	if err != nil {
+		return Measurement{}, fmt.Errorf("failed to initialize rest request: %w", err)
+	}
+	req.Header.Add("Accept", ACCEPT_MEASUREMENT)
+	req.SetBasicAuth(c.Username, c.Password)
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return Measurement{}, fmt.Errorf("failed to execute rest request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		switch resp.StatusCode {
+		case http.StatusUnauthorized:
+			return Measurement{}, generic.BadCredentialsErr
+		case http.StatusForbidden:
+			return Measurement{}, generic.AccessDeniedErr
+		default:
+			return Measurement{}, fmt.Errorf("received an unexpected status code: %v", resp.StatusCode)
+		}
+	}
+
+	measurementFromAPI := Measurement{}
+
+	if err = json.NewDecoder(resp.Body).Decode(&measurementFromAPI); err != nil {
+		return Measurement{}, fmt.Errorf("failed to unmarshal response body: %w", err)
+	}
+
+	return measurementFromAPI, nil
+}
+func (c Client) deleteMeasurement(id string) (Measurement, error) {
+	req, err := http.NewRequest(
+		http.MethodDelete,
+		fmt.Sprintf("%v%v", c.BaseURL, MEASUREMENTS_API),
+		nil,
+	)
+	if err != nil {
+		return Measurement{}, fmt.Errorf("failed to initialize rest request: %w", err)
+	}
+	req.Header.Add("Accept", ACCEPT_MEASUREMENT)
+	req.SetBasicAuth(c.Username, c.Password)
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return Measurement{}, fmt.Errorf("failed to execute rest request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		switch resp.StatusCode {
+		case http.StatusUnauthorized:
+			return Measurement{}, generic.BadCredentialsErr
+		case http.StatusForbidden:
+			return Measurement{}, generic.AccessDeniedErr
+		default:
+			return Measurement{}, fmt.Errorf("received an unexpected status code: %v", resp.StatusCode)
+		}
+	}
+
+	measurementFromAPI := Measurement{}
+
+	if err = json.NewDecoder(resp.Body).Decode(&measurementFromAPI); err != nil {
+		return Measurement{}, fmt.Errorf("failed to unmarshal response body: %w", err)
 	}
 
 	return measurementFromAPI, nil
