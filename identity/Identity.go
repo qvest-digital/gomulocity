bad7809ffd0be3cb431f2ded99fb3985d0ad74a6
Holger Schmitz <h.schmitz@tarent.de>
2020-07-16 05:33:05 +0200

    Add getIdentity

--- identity/Identity.go
+++ identity/Identity.go
@@ -1,37 +1,76 @@
 package identity
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+
 	"github.com/tarent/gomulocity/deviceinformation"
 	"github.com/tarent/gomulocity/generic"
 )
 
+const (
+	IDENTITY_TYPE = "application/vnd.com.nsn.cumulocity.identityApi+json"
+)
+
 type Identity struct {
-	self string
-	externalId string
+	self                 string
+	externalId           string
 	externalIdOfGlobalId string
 }
 
-type ExternalIDCollection{
-	self string
+type ExternalIDCollection struct {
+	self        string
 	externalIds []ExternalID
-	prev string
-	next string
+	prev        string
+	next        string
 }
 
 type ExternalID struct {
-	self string
-	externalId string
-	typ string
+	self          string
+	externalId    string
+	typ           string
 	managedObject deviceinformation.ManagedObject
 }
 
-type IdentityAPI interface{
-	GetIdentity()(Identity, *generic.Error)
+type IdentityAPI interface {
+	GetIdentity() (Identity, *generic.Error)
 	GetExternalIDCollection() //TODO
-	GetExternalID(externalID ExternalID)(ExternalID, *generic.Error)
-	CreateExternalID(ID ExternalID)(ExternalID, *generic.Error)
-	DeleteExternalID(externalID ExternalID)(*generic.Error)
+	GetExternalID(externalID ExternalID) (ExternalID, *generic.Error)
+	CreateExternalID(ID ExternalID) (ExternalID, *generic.Error)
+	DeleteExternalID(externalID ExternalID) *generic.Error
 }
-func NewIdentityAPI(client *generic.Client)IdentityAPI{
-	return IdentityAPI{client: client}
+
+type identityAPI struct {
+	basePath string
+	client   *generic.Client
+}
+
+func NewIdentityAPI(client *generic.Client, basePath string) identityAPI {
+	return identityAPI{
+		client:   client,
+		basePath: basePath,
+	}
+}
+
+func (i identityAPI) GetIdentity(identity Identity) (Identity, *generic.Error) {
+	body, status, err := i.client.Get(fmt.Sprintf("%s/%s", i.basePath, url.QueryEscape("identity")), generic.AcceptHeader(IDENTITY_TYPE))
+
+	if err != nil {
+		return Identity{}, generic.ClientError(fmt.Sprintf("Error while getting the Identity Ressource: %s", err.Error()), "Get")
+	}
+	if status == http.StatusNotFound {
+		return Identity{}, nil
+	}
+	if status != http.StatusOK {
+		return Identity{}, generic.CreateErrorFromResponse(body, status)
+	}
+	result := Identity{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return Identity{}, generic.ClientError(fmt.Sprintf("Error while parsing response JSON: %s", err.Error()), "ResponseParser")
+	}
+
+	return result, nil
 }
