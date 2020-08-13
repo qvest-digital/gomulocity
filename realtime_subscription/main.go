3b94c0de5afdbc0c5b8cf9a91e3736a4dffa761d
Holger Schmitz <h.schmitz@tarent.de>
2020-08-13 06:06:56 +0200

    refine structure, add types for requests and responses

--- realtime_subscription/main.go
+++ realtime_subscription/main.go
@@ -12,8 +12,104 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-// cumulocityURL/meta/handshake
+// buildingiot.test-ram.m2m.telekom.com/meta/handshake
 var addr = flag.String("addr", "echo.websocket.org", "http service address")
+
+type AuthRequest struct {
+	Id                       string
+	Channel                  string
+	Ext                      Login
+	Version                  string
+	MinimumVersion           string `json:"omitempty"`
+	SupportedConnectionTypes []string
+}
+
+type Login struct {
+	Authentification Auth `json:"com.cumulocity.authn"`
+	SystemOfUnits    string
+}
+
+type Auth struct {
+	Token     string
+	Tfa       string `json:"omitempty"`
+	XsrfToken string `json:"omitempty"`
+}
+
+type AuthResponse struct {
+	Id                       string
+	Channel                  string
+	Version                  string   `json:"omitempty"`
+	MinimumVersion           string   `json:"omitempty"`
+	SupportedConnectionTypes []string `json:"omitempty"`
+	ClientId                 string   `json:"omitempty"`
+	Successful               bool
+	error                    string `json:"omitempty"`
+}
+
+type SubscriptiobRequest struct {
+	Id           string
+	Channel      string
+	ClientId     string
+	Subscription string
+}
+
+type SubscriptionResponse struct {
+	Id           string
+	Channel      string
+	ClientId     string
+	Subscription string
+	Scuccessful  bool
+	Error        string
+}
+
+type ConnectRequest struct {
+	Id             string `json:"omitempty"`
+	Channel        string
+	ClientId       string
+	ConnectionType string
+	Advice         Advice `json:"omitempty"`
+}
+
+type Advice struct {
+	Timeout  int `json:"omitempty"`
+	Interval int `json:"omitempty"`
+}
+
+type ConnectResponse struct {
+	Id         string
+	Channel    string
+	ClientId   string
+	Successful bool
+	Data       []Notification
+	Error      string
+}
+
+type DisconnectRequest struct {
+	Id       string `json:"omitempty"`
+	Channel  string
+	ClientId string
+}
+
+type DisconnectResponse struct {
+	Id         string
+	Channel    string
+	Successful bool
+	ClientId   string
+	Error      string
+}
+
+var ext = AuthRequest{
+	Channel: "/meta/handshake",
+	Ext: Login{
+		Authentification: Auth{
+			Token: "login",
+		},
+		SystemOfUnits: "metric",
+	},
+	Version:                  "1.0",
+	MinimumVersion:           "1.0",
+	SupportedConnectionTypes: []string{"websocket"},
+}
 
 func main() {
 	flag.Parse()
@@ -21,6 +117,9 @@ func main() {
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
+
+	response := make(chan []byte, 5)
+	defer close(response)
 
 	u := url.URL{Scheme: "ws", Host: *addr}
 	log.Printf("connecting to %s", u.String())
@@ -33,9 +132,6 @@ func main() {
 	defer c.Close()
 
 	done := make(chan struct{})
-
-	response := make(chan []byte, 5)
-	defer close(response)
 
 	go func() {
 		defer close(done)
@@ -52,13 +148,6 @@ func main() {
 
 	send := make(chan []byte, 5)
 	defer close(send)
-	ext := `{
-		"ext": {
-		"com.cumulocity.authn": {
-		"token": "credentials",
-		},
-		"systemOfUnits": "metric"
-	}`
 
 	go func() {
 		for {
@@ -91,13 +180,15 @@ func main() {
 			}
 		}
 	}()
-	type handshake struct {
-		clientId string
+
+	out, err := json.Marshal(ext)
+	if err != nil {
+		log.Fatalf("failed to Marshal: %s", err)
 	}
-	send <- []byte(ext)
+	send <- out
 	time.Sleep(3 * time.Second)
 	answer := <-response
-	respFromHandskake := handshake{}
-	json.Unmarshal(answer, respFromHandskake)
-	println(respFromHandskake.clientId)
+	respFromHandshake := AuthResponse{}
+	json.Unmarshal(answer, respFromHandshake)
+	println(respFromHandskake.ClientId)
 }
